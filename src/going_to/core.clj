2be185415a420ce2ef046b86b89c0e4043500616
trikitrok <trikitrok@gmail.com>
2015-07-29 16:31:24 +0200

    Memoized factorial computations

--- src/going_to/core.clj
+++ src/going_to/core.clj
@@ -1,13 +1,18 @@
 (ns going-to.core)
 
+(defn- factorial-helper [n acc]
+  (if (or (zero? n) (= 1 n))
+    acc
+    (recur (dec n) (* acc n))))
+
+(def memoized-factorial-helper (memoize factorial-helper))
+
 (defn factorial [n]
-  (loop [n n acc 1N]
-    (if (or (zero? n) (= 1 n))
-      acc
-      (recur (dec n) (* acc n)))))
+  (memoized-factorial-helper n 1N))
 
 (defn going
   "(1 / n!) * (1! + 2! + 3! + ... + n!)"
   [n]
   (/ (reduce + (map factorial (range 1 (inc n))))
      (factorial n)))
+
