8852e68f3842ee04a2ba909c4fb029a5495eb512
trikitrok <trikitrok@gmail.com>
2015-07-29 16:16:14 +0200

    First working version of going

--- src/going_to/core.clj
+++ src/going_to/core.clj
@@ -5,3 +5,9 @@
     (if (or (zero? n) (= 1 n))
       acc
       (recur (dec n) (* acc n)))))
+
+(defn going
+  "(1 / n!) * (1! + 2! + 3! + ... + n!)"
+  [n]
+  (/ (reduce + (map factorial (range 1 (inc n))))
+     (factorial n)))
